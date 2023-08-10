893e2a95592f07764574acd2058136d07e624641
Carlin Mitchell <carlinmi@umn.edu>
2023-08-10 13:27:19 -0500

    deploy-demo

---
+++ src/components/managers/uiManager/windowEventListeners.js
@@ -0,0 +1,23 @@
+import { mobileMenuUp } from "./mobileMenu-ui";
+import { debounce } from "../../utils";
+
+const breakPoints = {
+  xs: 0,
+  sm: 480,
+  md: 720,
+  lg: 960,
+  xl: 1200,
+};
+
+export function applyWindowListeners() {
+  window.onresize = debounce(function (e) {
+    const { innerWidth } = e.target;
+
+    if (innerWidth >= breakPoints.md) {
+      const mobileMenu = document.querySelector(".mobile-menu");
+      if (mobileMenu.classList.contains("menu-down")) {
+        mobileMenuUp();
+      }
+    }
+  });
+}
