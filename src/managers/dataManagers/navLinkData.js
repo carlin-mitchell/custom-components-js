a68319152cd2210a4d73aaefc8849c3b8b8bcc88
Carlin Mitchell <carlin.d.mitchell@gmail.com>
2023-08-10 22:04:44 -0500

    “sync”

--- src/managers/dataManagers/navLinkData.js
+++ src/managers/dataManagers/navLinkData.js
@@ -3,8 +3,14 @@ export const navLinkElementInfo = [
     innerText: "contact",
     href: "#",
     subLinkObjects: [
-      { innerText: "Link 1", href: "#" },
-      { innerText: "Link 2", href: "#" },
+      {
+        innerText: "Github Repo",
+        href: "https://github.com/carlin-mitchell/custom-components-js",
+      },
+      {
+        innerText: "Github Live",
+        href: "https://carlin-mitchell.github.io/custom-components-js/",
+      },
       { innerText: "Link 3", href: "#" },
       { innerText: "Link 4", href: "#" },
     ],
