d2abde40d3ddfb898cc774fa9a26d0609f1219f7
Carlin Mitchell <carlinmi@umn.edu>
2023-08-18 13:15:35 -0500

    “sync”

--- src/managers/uiManager/carousel-ui.js
+++ src/managers/uiManager/carousel-ui.js
@@ -6,6 +6,39 @@ function getAllCarouselNavButtons() {
   return [...document.querySelectorAll(".carousel-indicator")];
 }
 
+function getPreviousSlideIndex() {
+  let previousSlideIndex = getCurrentSlideIndex() - 1;
+
+  const correctedIndex =
+    previousSlideIndex < 0
+      ? getAllCarouselSlideIds().length - 1
+      : previousSlideIndex;
+
+  return correctedIndex;
+}
+
+function getNextSlideIndex() {
+  let nextSlideIndex = getCurrentSlideIndex() + 1;
+
+  const correctedIndex =
+    nextSlideIndex > getAllCarouselSlideIds().length - 1 ? 0 : nextSlideIndex;
+
+  return correctedIndex;
+}
+
+function getAllCarouselSlideIds() {
+  getAllCarouselSlides().map((elem) => elem.id);
+}
+
+function getCurrentSlideIndex() {
+  const allSlideIds = getAllCarouselSlideIds();
+  const currentSlideId = document.querySelector(".carousel-slide.current").id;
+
+  const currentSlideIndex = allSlideIds.indexOf(currentSlideId);
+
+  return currentSlideIndex;
+}
+
 // move right when clicking on the right button
 export function clickCarouselRight() {
   console.log("carousel right");
